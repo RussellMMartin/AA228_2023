d3efcc84bc2bac697a3b10d04222ae4fc2dfbbbf
rumartin <rumartin@stanford.edu>
2023-02-23 09:56:51 -0800

    aa228 proj 1 complete

--- project1/project1.py
+++ project1/project1.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import scipy.special
 import copy
 from datetime import datetime
+from networkx.drawing.nx_pydot import graphviz_layout
 
 def write_gph(dag, idx2names, filename):
     with open(filename, 'w') as f:
@@ -98,21 +99,28 @@ def compute(infile, outfile):
     D = np.array(D).T - 1 # subtract 1 for 0-indexing
     n = len(vars)
 
-    # (2) get a graph
+    # (2) get a graph, set nodes to 0, 1, ..., n
     G = nx.DiGraph()
-    G.add_nodes_from(range(n))
+    G.add_nodes_from(range(n)) 
     graphInfo = getGraphInfo(n, G, D) 
     
     # (3) score the graph
     score = bayes_score(vars, G, D, graphInfo)
+    allTimeBest = {'G': G, 'score': score}
 
     # (4) optimize the graph using local directed graph search
+    maxTime = 20 # mins
     maxAttempts = 1000
+    undoRate = np.floor(maxAttempts/10)
+    resetRate = np.floor(maxAttempts/5)
     consecutiveNewGraphsWithoutImprovement = 0
+    consecutiveNewGraphsWithoutAllTimeImprovement = 0
     totalGraphsGenerated = 0
-    scores = [score]
+    scores = np.array([[0, score]])
+    mostRecentChange = {'type': None, 'nodes': None}
     while True:
 
+        runtime_mins = np.round((datetime.now() - startt).total_seconds()/60, 2)
         G_new = copy.deepcopy(G)
         existingEdges = list(G_new.edges)
         # (4.1) add an edge
@@ -129,6 +137,7 @@ def compute(infile, outfile):
 
                 # add to graph. If we still have a DAG, we can go get the score
                 G_new.add_edge(startNode, endNode)
+                
                 if nx.is_directed_acyclic_graph(G_new):
                     break
         
@@ -152,25 +161,70 @@ def compute(infile, outfile):
         if nx.is_directed_acyclic_graph(G_new):
             graphInfo_new = getGraphInfo(n, G_new, D) 
             score_new = bayes_score(vars, G_new, D, graphInfo_new)
+            totalGraphsGenerated += 1
 
-            if score_new > score:
+            if score_new > score or np.random.randint(0, 20) == 0:
+                if score_new > score:
+                    consecutiveNewGraphsWithoutImprovement = 0
+                if score_new > allTimeBest['score']:
+                    improvement = round(score_new-allTimeBest['score'],2)
+                    print(f'New all time best score! {round(score_new,2)} (change: {improvement})')
+                    allTimeBest['score'] = score_new
+                    allTimeBest['G'] = G_new
+                    consecutiveNewGraphsWithoutAllTimeImprovement = 0
+                    
                 G = copy.deepcopy(G_new)
                 score = copy.deepcopy(score_new)
-                consecutiveNewGraphsWithoutImprovement = 0
-                scores.append(score_new)
-                print(f'New best score {score}. Runtime = {datetime.now() - startt} \n')
+                scores = np.append(scores, np.array([[runtime_mins,score]]), axis=0)
+                mostRecentChange['type'] = decision
+                mostRecentChange['nodes'] = [startNode, endNode]
+                print(f'Improved score {round(score,2)}. Runtime = {runtime_mins}')
             else:
                 consecutiveNewGraphsWithoutImprovement += 1
+                consecutiveNewGraphsWithoutAllTimeImprovement += 1
         else: 
             consecutiveNewGraphsWithoutImprovement += 1
+            consecutiveNewGraphsWithoutAllTimeImprovement += 1
+
+        # (4.4) print status 
+        if np.mod(consecutiveNewGraphsWithoutAllTimeImprovement, 100) == 0 and consecutiveNewGraphsWithoutAllTimeImprovement > 99:
+            print(f'consective graphs without all-time score improvement  = {consecutiveNewGraphsWithoutAllTimeImprovement}' +
+            f' runtime = {runtime_mins}')
+
+        # (4.5) if no changes seem to help, undo the last change
+        if np.mod(consecutiveNewGraphsWithoutImprovement, undoRate) == undoRate-1:
+            print(f'No changes are helping - removing last change')
+            if mostRecentChange['type'] == 0: # undo an add
+                try:
+                    G.remove_edge(mostRecentChange['nodes'][0], mostRecentChange['nodes'][1])
+                except:
+                    print(f'error, cannot remove nodes from graph')
+            elif mostRecentChange['type'] == 1: # undo a remove
+                try:
+                    G.add_edge(mostRecentChange['nodes'][0], mostRecentChange['nodes'][1])
+                except:
+                    print(f'error, cannot add nodes to graph')
+            elif mostRecentChange['type'] == 2: # undo a flip
+                try:
+                    G.remove_edge(mostRecentChange['nodes'][1], mostRecentChange['nodes'][0])
+                    G.add_edge(mostRecentChange['nodes'][0], mostRecentChange['nodes'][1])
+                except:
+                    print(f'error, cannot flip nodes')
         
-        # (4.4) if we've had too many consecutive duds, we're done
-        if consecutiveNewGraphsWithoutImprovement > maxAttempts:
-            print(f'All done, runtime = {datetime.now() - startt} \n')
+        
+        if np.mod(consecutiveNewGraphsWithoutAllTimeImprovement, resetRate) == resetRate-1:
+            print(f'{resetRate} consecutive graphs w/o all-time improvement - resetting back to all-time G')
+            G = copy.deepcopy(allTimeBest['G'])
+            
+        
+        # (4.6) if we've had too many consecutive duds, we're done
+        if consecutiveNewGraphsWithoutImprovement > maxAttempts/10 or consecutiveNewGraphsWithoutAllTimeImprovement > maxAttempts or runtime_mins > maxTime:
             break
 
-    # (5) return the graph when satisfactory
+    # (5) return the graph when done
     network_str = ''
+    G = copy.deepcopy(allTimeBest['G'])
+    
     for i in range(len(vars)):
         parentName = vars[i]
         kidsIdxs = list(G.successors(i))
@@ -180,10 +234,35 @@ def compute(infile, outfile):
                 network_str += strToAdd
         else:
             network_str += f'{parentName}, \n'
-    print(f'score = {score}')
+    graphsPerMin = round(totalGraphsGenerated/runtime_mins, 2)
+    print(f'generated {totalGraphsGenerated} graphs in {runtime_mins} mins ({graphsPerMin} graphs/min), best score = {score}')
     print(network_str)
-    plt.plot(np.array(scores))
-    plt.show()
+
+    if 1:
+        plt.figure()
+        plt.plot(scores[:,0], scores[:,1])
+        plt.xlabel('time (mins)')
+        plt.ylabel('graph bayesian score')
+        plt.title(f'{infile} score vs time')
+
+        nodeMapping = dict(zip(list(range(len(vars))), list(vars)))
+        G = nx.relabel_nodes(G, nodeMapping)
+        options = {
+            "font_size": 10,
+            "node_size": 1000,
+            "node_color": "white",
+            "edgecolors": "gray",
+            "linewidths": 5,
+            "width": 5,
+        }
+        plt.figure()
+        # pos = nx.shell_layout(G)
+        pos = graphviz_layout(G, prog='dot')
+        nx.draw_networkx(G, pos=pos, arrows=True, arrowstyle='->', **options)
+        plt.title(f'{infile} graph')
+        plt.axis("off")
+        plt.show()
+    print('done')
 
 def main():
     
@@ -199,6 +278,6 @@ if __name__ == '__main__':
     if 0:
         main()
     else:
-        inputfilename = "data/small.csv"
-        outputfilename = "graphs/small.gph"
+        inputfilename = "data/large.csv"
+        outputfilename = "graphs/large.gph"
         compute(inputfilename, outputfilename)
